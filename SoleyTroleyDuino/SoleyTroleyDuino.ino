39db5f2e35e29f4d4f235793d772a774213bdf61
CaptainCredible <cpt.credible@gmail.com>
2018-02-19 14:50:06 +0100

    Add project files.

---
+++ SoleyTroleyDuino/SoleyTroleyDuino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SoleyTroleyDuino.ino
+ Created:	2/19/2018 2:50:05 PM
+ Author:	Stoodio 2nd
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
